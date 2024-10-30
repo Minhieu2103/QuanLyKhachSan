c89e58d4c499f54dfbe0bc469b659448c6f6300e
HieuNguyen <Minhieu2k33@gmail.com>
2024-10-30 14:15:19 +0700

    code dang ki

--- Nhom12_/Nhom12_/DangKy.Designer.cs
+++ Nhom12_/Nhom12_/DangKy.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.Label label5;
+            System.Windows.Forms.Label label2;
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtXNMatKhau = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtTenTk = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.btnDangKy = new System.Windows.Forms.Button();
             this.btnHuyDK = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.panel5 = new System.Windows.Forms.Panel();
-            label5 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,7 +56,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(978, 406);
+            this.panel1.Size = new System.Drawing.Size(1378, 406);
             this.panel1.TabIndex = 1;
             // 
             // pictureBox1
@@ -65,20 +65,119 @@
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(978, 406);
+            this.pictureBox1.Size = new System.Drawing.Size(1378, 406);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // panel2
+            // label2
             // 
-            this.panel2.Controls.Add(this.btnDangKy);
-            this.panel2.Controls.Add(this.btnHuyDK);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 902);
-            this.panel2.Margin = new System.Windows.Forms.Padding(6);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(978, 135);
-            this.panel2.TabIndex = 4;
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label2.Location = new System.Drawing.Point(184, 181);
+            label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(163, 37);
+            label2.TabIndex = 0;
+            label2.Text = "Mật Khẩu:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtEmail);
+            this.panel3.Controls.Add(this.txtXNMatKhau);
+            this.panel3.Controls.Add(this.txtMatKhau);
+            this.panel3.Controls.Add(this.txtTenTk);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 406);
+            this.panel3.Margin = new System.Windows.Forms.Padding(6);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1378, 496);
+            this.panel3.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(358, 400);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(6);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(458, 44);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txtXNMatKhau
+            // 
+            this.txtXNMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtXNMatKhau.Location = new System.Drawing.Point(358, 285);
+            this.txtXNMatKhau.Margin = new System.Windows.Forms.Padding(6);
+            this.txtXNMatKhau.Name = "txtXNMatKhau";
+            this.txtXNMatKhau.PasswordChar = '*';
+            this.txtXNMatKhau.Size = new System.Drawing.Size(458, 44);
+            this.txtXNMatKhau.TabIndex = 1;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhau.Location = new System.Drawing.Point(358, 162);
+            this.txtMatKhau.Margin = new System.Windows.Forms.Padding(6);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.PasswordChar = '*';
+            this.txtMatKhau.Size = new System.Drawing.Size(458, 44);
+            this.txtMatKhau.TabIndex = 1;
+            // 
+            // txtTenTk
+            // 
+            this.txtTenTk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenTk.Location = new System.Drawing.Point(358, 44);
+            this.txtTenTk.Margin = new System.Windows.Forms.Padding(6);
+            this.txtTenTk.Name = "txtTenTk";
+            this.txtTenTk.Size = new System.Drawing.Size(458, 44);
+            this.txtTenTk.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(242, 412);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 37);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Email:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 296);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(314, 37);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Xác Nhận Mật Khẩu:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 56);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên Đăng Nhập:";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btnDangKy);
+            this.panel5.Controls.Add(this.btnHuyDK);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 902);
+            this.panel5.Margin = new System.Windows.Forms.Padding(6);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1378, 135);
+            this.panel5.TabIndex = 10;
             // 
             // btnDangKy
             // 
@@ -90,6 +189,7 @@
             this.btnDangKy.TabIndex = 0;
             this.btnDangKy.Text = "Đăng Ký";
             this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click_1);
             // 
             // btnHuyDK
             // 
@@ -101,121 +201,23 @@
             this.btnHuyDK.TabIndex = 0;
             this.btnHuyDK.Text = "Hủy";
             this.btnHuyDK.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label5.Location = new System.Drawing.Point(184, 181);
-            label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            label5.Name = "label5";
-            label5.Size = new System.Drawing.Size(163, 37);
-            label5.TabIndex = 0;
-            label5.Text = "Mật Khẩu:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(358, 400);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(458, 44);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(358, 285);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(458, 44);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(358, 162);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(6);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.PasswordChar = '*';
-            this.textBox3.Size = new System.Drawing.Size(458, 44);
-            this.textBox3.TabIndex = 1;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(358, 44);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(6);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(458, 44);
-            this.textBox4.TabIndex = 1;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(242, 412);
-            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(106, 37);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "Email:";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(36, 296);
-            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(314, 37);
-            this.label7.TabIndex = 0;
-            this.label7.Text = "Xác Nhận Mật Khẩu:";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(96, 56);
-            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(254, 37);
-            this.label8.TabIndex = 0;
-            this.label8.Text = "Tên Đăng Nhập:";
-            // 
-            // panel5
-            // 
-            this.panel5.Controls.Add(this.textBox1);
-            this.panel5.Controls.Add(this.textBox2);
-            this.panel5.Controls.Add(this.textBox3);
-            this.panel5.Controls.Add(this.textBox4);
-            this.panel5.Controls.Add(this.label6);
-            this.panel5.Controls.Add(this.label7);
-            this.panel5.Controls.Add(label5);
-            this.panel5.Controls.Add(this.label8);
-            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel5.Location = new System.Drawing.Point(0, 406);
-            this.panel5.Margin = new System.Windows.Forms.Padding(6);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(978, 496);
-            this.panel5.TabIndex = 6;
+            this.btnHuyDK.Click += new System.EventHandler(this.btnHuyDK_Click);
             // 
             // DangKy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(978, 1052);
-            this.Controls.Add(this.panel2);
+            this.ClientSize = new System.Drawing.Size(1378, 1387);
             this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.Name = "DangKy";
             this.Text = "DangKy";
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.panel5.ResumeLayout(false);
-            this.panel5.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -224,16 +226,16 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtXNMatKhau;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtTenTk;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Button btnDangKy;
         private System.Windows.Forms.Button btnHuyDK;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Panel panel5;
     }
 }
--- Nhom12_/Nhom12_/DangKy.cs
+++ Nhom12_/Nhom12_/DangKy.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Nhom12_.ClassLogin;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,45 @@ namespace Nhom12_
         public DangKy()
         {
             InitializeComponent();
+        }
+        public bool checkAccount(string ac) // check mật khẩu và tài khoản 
+        {
+            return Regex.IsMatch(ac, "^[a-zA-Z0-9]{6,24}$");
+        }
+
+        public bool checkEmail(string email) // kiểm tra email người dùng nhập có hợp lệ hong
+        {
+            return Regex.IsMatch(email, @"^[a-zA-Z0-9_.]{3,20}@gmail.com(.vn|)$");
+        }
+        Modify modify = new Modify();   
+     
+
+        private void btnDangKy_Click_1(object sender, EventArgs e)
+        {
+            string tentk = txtTenTk.Text;
+            string makhau = txtMatKhau.Text;
+            string xnmatkhau = txtXNMatKhau.Text;
+            string email = txtEmail.Text;
+            if (!checkEmail(email)) { MessageBox.Show("Nhập sai Email", "Thông Báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning); return; }
+            if (!checkAccount(tentk)) { MessageBox.Show("Tên Tài Khoản Không Hợp Lệ! Nhập Lại", "Thông Báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning); return; }
+            if (xnmatkhau != makhau) { MessageBox.Show("Vui Lòng Xác Nhận Mật Khẩu ", "Thông Báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning); return; }
+            if (modify.TaiKhoans("Select * from TaiKhoan where Email= '" + email + "'").Count != 0) { MessageBox.Show("Email đã được đăng ký vui lòng đổi email khác", "Thông Báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning); return; }
+            try
+            {
+                string squery = "Insert into TaiKhoan values ('" + tentk + "','" + makhau + "','" + email + "')";
+                modify.Command(squery);
+
+            }
+            catch
+            {
+                MessageBox.Show("Tài Khoản Đã Tồn Tại, Vui Lòng Nhập Tên Tài Khoản Khác", "Thông Báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            this.Close();
+        }
+
+        private void btnHuyDK_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
